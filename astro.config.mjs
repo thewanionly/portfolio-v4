4c7fb8cfcf8c11d88700e680cb541e8e6439fd64
thewanionly <pelloani@gmail.com>
2025-04-04 04:17:34 +0800

    feat: add social icon links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
+  integrations: [
+    react(),
+    icon({
+      iconDir: 'src/assets/icons',
+    }),
+  ],
 });
