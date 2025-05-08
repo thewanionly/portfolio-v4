0b3c0ee5e2913b93336b9698dffdeeed7fc8f81d
thewanionly <pelloani@gmail.com>
2025-05-09 05:29:06 +0800

    feat: implement TextAreaWithLabel

---
+++ src/components/TextAreaWithLabel/TextAreaWithLabel.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
+import { Textarea } from '../ui/textarea';
+
+interface TextAreaWithLabelProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+  type?: string;
+  id: string;
+  rows?: number;
+}
+
+export const TextAreaWithLabel = ({
+  label,
+  value,
+  onChange,
+  placeholder = '',
+  id,
+  rows,
+}: TextAreaWithLabelProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const shouldFloat = isFocused || value.length > 0;
+
+  return (
+    <div className="grid relative w-full ">
+      <Label
+        htmlFor={id}
+        className={cn(
+          'absolute left-6 text-muted-foreground transition-all pointer-events-none uppercase',
+          'ml-[2px]',
+          shouldFloat ? 'text-xs -top-4' : 'text-base top-2'
+        )}
+      >
+        {label}
+      </Label>
+      <Textarea
+        id={id}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        placeholder={shouldFloat ? placeholder : ''}
+        rows={rows}
+        autoComplete="off"
+        className={cn(
+          'border-0 border-b-2 border-input rounded-none px-6 resize-none',
+          'focus-visible:ring-0 focus-visible:border-b-2 focus-visible:border-primary',
+          'bg-transparent'
+        )}
+      />
+    </div>
+  );
+};
