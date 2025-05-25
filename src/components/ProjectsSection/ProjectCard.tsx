208a968a57e0342cce15cd2c99c88ddb5e222f99
thewanionly <pelloani@gmail.com>
2025-05-26 04:22:55 +0800

    feat: support images in ProjectCard

--- src/components/ProjectsSection/ProjectCard.tsx
+++ src/components/ProjectsSection/ProjectCard.tsx
@@ -2,16 +2,16 @@ import { cn } from '@/lib/utils';
 import type { Project } from './ProjectsSection.types';
 import { buttonVariants } from '../ui/button';
 
-// import sampleImg from '../../assets/images/entertainment-web-app-wani.png';
-
 export const ProjectCard = ({
-  project: { title, technologies, projectUrl, sourceCodeUrl },
+  project: { imgSrc, imgAlt, title, technologies, projectUrl, sourceCodeUrl },
 }: {
   project: Project;
 }) => {
   return (
     <div>
-      <div className="mb-5">{/* <img src={sampleImg.src} alt={'A sample image'} /> */}</div>
+      <div className="mb-5">
+        <img className=" object-cover aspect-[1.5]" src={imgSrc} alt={imgAlt} loading="lazy" />
+      </div>
       <p className="text-2xl font-bold leading-[1.3] mb-2">{title}</p>
       <div className="flex flex-wrap gap-x-4.5 text-body text-lg leading-[1.5] font-medium mb-5">
         {technologies.map((tech) => (
