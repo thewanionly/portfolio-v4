8cf65dc64a7df597fb58fefb95044ad0a1276151
thewanionly <pelloani@gmail.com>
2025-05-27 13:50:26 +0800

    feat: update technologies styling

--- src/components/ProjectsSection/ProjectCard.tsx
+++ src/components/ProjectsSection/ProjectCard.tsx
@@ -12,10 +12,12 @@ export const ProjectCard = ({
       <div className="mb-5">
         <img className=" object-cover aspect-[1.5]" src={imgSrc} alt={imgAlt} loading="lazy" />
       </div>
-      <p className="text-2xl font-bold leading-[1.3] mb-2">{title}</p>
-      <div className="flex flex-wrap gap-x-4.5 text-body text-lg leading-[1.5] font-medium mb-5">
+      <p className="text-2xl font-bold leading-[1.3] mb-4">{title}</p>
+      <div className="flex flex-wrap gap-2 text-sm font-medium mb-8 text-grey-light">
         {technologies.map((tech) => (
-          <span key={tech}>{tech}</span>
+          <span key={tech} className="px-2.5 py-1 rounded-full bg-grey-light/10">
+            {tech}
+          </span>
         ))}
       </div>
       <div className="flex gap-6 justify-between">
