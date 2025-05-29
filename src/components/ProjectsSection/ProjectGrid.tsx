99b3f7551052fb33f98c76473690e87ba86c907f
thewanionly <pelloani@gmail.com>
2025-05-29 14:49:57 +0800

    feat: styling updates to ProjectGrid

--- src/components/ProjectsSection/ProjectGrid.tsx
+++ src/components/ProjectsSection/ProjectGrid.tsx
@@ -3,7 +3,7 @@ import type { Project } from './ProjectsSection.types';
 
 export const ProjectGrid = ({ projects }: { projects: Project[] }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-15">
       {projects.map((project) => (
         <ProjectCard key={project.id} project={project} />
       ))}
