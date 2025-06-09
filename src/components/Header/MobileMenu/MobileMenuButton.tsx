70de6a208a7bb5910c6e5f4a31ca52659dbd7cfd
thewanionly <pelloani@gmail.com>
2025-06-09 14:24:03 +0800

    feat: update icon logic of MobileMenuButton

--- src/components/Header/MobileMenu/MobileMenuButton.tsx
+++ src/components/Header/MobileMenu/MobileMenuButton.tsx
@@ -3,13 +3,12 @@ import { Button } from '../../ui/button';
 import { useMobileMenuStore } from './MobileMenu.store';
 
 export const MobileMenuButton = () => {
-  const isOpen = useMobileMenuStore((state) => state.isOpen);
-  const toggleIsOpen = useMobileMenuStore((state) => state.toggleIsOpen);
+  const { isOpen, toggleIsOpen } = useMobileMenuStore();
 
   return (
     <Button className="md:hidden" variant="ghost" size="icon" onClick={toggleIsOpen}>
-      {isOpen ? <Menu className="size-5 md:size-6" /> : <X className="size-5 md:size-6" />}
-      <span className="sr-only">Open menu</span>
+      {!isOpen ? <Menu className="size-5 md:size-6" /> : <X className="size-5 md:size-6" />}
+      <span className="sr-only">{!isOpen ? 'Open' : 'Close'} menu</span>
     </Button>
   );
 };
